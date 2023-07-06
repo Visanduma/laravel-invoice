3db25292257782056b44b10a3985237a6d88ff11
LaHiRu <lahirulhr@gmail.com>
2023-07-06 10:41:19 +0530

    Currency issue

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,16 +1,3 @@
 <?php
 
-if (!function_exists('money')) {
 
-    function money($amount, $symbol = false)
-    {
-
-        $currencySymbol = \Visanduma\LaravelInvoice\Helpers\MoneyFormatter::getCurrency();
-
-        if (!$symbol) {
-            return number_format($amount, 2);
-        }
-
-        return $currencySymbol . number_format($amount, 2);
-    }
-}
