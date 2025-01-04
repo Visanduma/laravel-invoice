c3c826f3ab03fc289177fc6c133b386978ecb559
Dumith Salinda <dumithsalinda@users.noreply.github.com>
2025-01-04 23:25:19 +0530

    Fixed index length issue

--- database/migrations/update_invoice_items.php
+++ database/migrations/update_invoice_items.php
@@ -14,7 +14,9 @@ class UpdateInvoiceItems extends Migration
     public function up()
     {
         Schema::table('laravel_invoice_items', function (Blueprint $table) {
-            $table->morphs('invoiceItemable');
+            $table->string('invoiceItemable_type');
+            $table->unsignedBigInteger('invoiceItemable_id');
+            $table->index(['invoiceItemable_type', 'invoiceItemable_id'], 'invoice_itemable_index');
             $table->unsignedBigInteger('parent_item_id')->nullable()->after('id');
             $table->foreign('parent_item_id')->references('id')->on('laravel_invoice_items')->onDelete('cascade');
         });
